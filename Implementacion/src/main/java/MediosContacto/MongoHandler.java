344ceba69e9c8323601686ce08e1254ccc2ec609
rominamartinez99 <romimartinez5799@gmail.com>
2021-07-27 12:56:20 -0300

    mongo andando

--- Implementacion/src/main/java/MediosContacto/MongoHandler.java
+++ Implementacion/src/main/java/MediosContacto/MongoHandler.java
@@ -1,15 +1,12 @@
 package MediosContacto;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.ServerAddress;
+import com.mongodb.*;
 
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 
 import org.bson.Document;
 import java.util.Arrays;
-import com.mongodb.Block;
 
 import com.mongodb.client.MongoCursor;
 import static com.mongodb.client.model.Filters.*;
@@ -20,34 +17,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MongoHandler {
-    MongoClient mongoClient;
     MongoDatabase database;
-    MongoCollection<Document> collection;
 
-
-    public void iniciarConexion(){
-        mongoClient = new MongoClient("localhost");
-        database = mongoClient.getDatabase("logs");
+    public static void main (String[] args){
+        MongoClient mongoClient = iniciarConexion();
+        MongoDatabase database = mongoClient.getDatabase("prueba");
+        insertarRegistro(database, "nombres","romina");
     }
+
+
+    public static MongoClient iniciarConexion(){
+        MongoClient mongoClient = new MongoClient("localhost",27017);
+        return mongoClient;
+    }
+
 
     public void crearColeccion(String collectionName){
         database.createCollection(collectionName);
     }
 
-    public void obtenerDb(String dbName){
-        database = mongoClient.getDatabase(dbName);
+    public static void insertarRegistro(MongoDatabase db, String collectionName, String nombre){
+
+        MongoCollection<Document> collection = db.getCollection(collectionName);
+        Document documento = new Document();
+        documento.put("nombre",nombre);
+
+        collection.insertOne(documento);
     }
 
-    public void traerColeccion(String collectionName){
-        collection = database.getCollection(collectionName);
-    }
-
-    public void insertarUnDocumento(Document doc){
-        collection.insertOne(doc);
-    }
-
-    public void insertarMuchosDocumentos(List<Document> documents){
-        collection.insertMany(documents);
-    }
     
 }
