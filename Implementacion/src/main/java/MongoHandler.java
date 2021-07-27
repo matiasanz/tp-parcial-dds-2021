4a45ad6f2091f7ccb617ca6ad6dfefe938251067
rominamartinez99 <romimartinez5799@gmail.com>
2021-07-26 22:54:29 -0300

    agrego mongo con metodos basicos, pom actualizado

---
+++ Implementacion/src/main/java/MongoHandler.java
@@ -0,0 +1,43 @@
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoHandler {
+    MongoClient mongoClient;
+    MongoDatabase database;
+    MongoCollection<Document> collection;
+
+
+    public void iniciarConexion(){
+        mongoClient = new MongoClient("localhost");
+        database = mongoClient.getDatabase("logs");
+    }
+
+    public void traerColeccion(String collectionName){
+        collection = database.getCollection(collectionName);
+    }
+
+    public void insertarUnDocumento(Document doc){
+        collection.insertOne(doc);
+    }
+
+    public void insertarMuchosDocumentos(List<Document> documents){
+        collection.insertMany(documents);
+    }
+    
+}
