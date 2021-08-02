7fdbe62e0e67c7edc8b1cd7ece8869f8c8e6ab2e
rominamartinez99 <romimartinez5799@gmail.com>
2021-08-02 16:15:51 -0300

    converter

--- Implementacion/src/main/java/Utils/LocalDateTimeConverter.java
+++ Implementacion/src/main/java/Utils/LocalDateTimeConverter.java
@@ -3,20 +3,24 @@ package Utils;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.sql.Date;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 @Converter(autoApply = true)
-public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Date> {
+public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
 
     @Override
-    public Date convertToDatabaseColumn(LocalDateTime locDateTime) {
-        return locDateTime == null ? null : java.sql.Date
-                .valueOf(locDateTime.toLocalDate());
+    public Timestamp convertToDatabaseColumn(LocalDateTime locDateTime) {
+
+        return Timestamp.valueOf(locDateTime);
     }
 
     @Override
-    public LocalDateTime convertToEntityAttribute(Date sqlDate) {
-        return sqlDate == null ? null : LocalDateTime.from(sqlDate.toLocalDate());
+    public LocalDateTime convertToEntityAttribute(Timestamp sqlDate) {
+        return sqlDate == null ? null : LocalDateTime.from(sqlDate.toLocalDateTime());
     }
 }
